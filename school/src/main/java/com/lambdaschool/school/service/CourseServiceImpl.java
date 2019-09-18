a2c42055e736b8c55b6e518f5a5bdcb74655b8c4
patrick-gordon <patrickgordon42@yahoo.com>
2019-09-18 16:00:26 -0500

    made it pagebale and sortable, need to do custom documentation

--- school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
+++ school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
@@ -25,6 +25,8 @@ public class CourseServiceImpl implements CourseService
         return list;
     }
 
+    
+
     @Override
     public ArrayList<CountStudentsInCourses> getCountStudentsInCourse()
     {
