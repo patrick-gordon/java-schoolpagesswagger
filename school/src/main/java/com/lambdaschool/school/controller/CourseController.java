a99780c677df3c31b2b37895d053d163f70936c5
patrick-gordon <patrickgordon42@yahoo.com>
2019-09-18 15:58:22 -0500

    made it pagebale and sortable, need to do custom documentation

--- school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -4,6 +4,8 @@ import com.lambdaschool.school.model.Course;
 import com.lambdaschool.school.service.CourseService;
 import com.lambdaschool.school.view.CountStudentsInCourses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,27 +14,26 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = "/courses")
-public class CourseController
-{
+public class CourseController {
     @Autowired
     private CourseService courseService;
 
     @GetMapping(value = "/courses", produces = {"application/json"})
-    public ResponseEntity<?> listAllCourses()
+    public ResponseEntity<?> listAllCourses(@PageableDefault(page = 0,
+                                                        size = 3)
+                                                        Pageable pageable)
     {
-        ArrayList<Course> myCourses = courseService.findAll();
+        ArrayList<Course> myCourses = courseService.findAll(pageable);
         return new ResponseEntity<>(myCourses, HttpStatus.OK);
     }
 
     @GetMapping(value = "/studcount", produces = {"application/json"})
-    public ResponseEntity<?> getCountStudentsInCourses()
-    {
+    public ResponseEntity<?> getCountStudentsInCourses() {
         return new ResponseEntity<>(courseService.getCountStudentsInCourse(), HttpStatus.OK);
     }
 
     @DeleteMapping("/courses/{courseid}")
-    public ResponseEntity<?> deleteCourseById(@PathVariable long courseid)
-    {
+    public ResponseEntity<?> deleteCourseById(@PathVariable long courseid) {
         courseService.delete(courseid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
